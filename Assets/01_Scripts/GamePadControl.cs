8540862c58a3897acd109c5ae98392f3d95bd28c
DongJun Hwang <ojb0012@naver.com>
2023-11-12 02:50:20 +0900

    room

---
+++ Assets/01_Scripts/GamePadControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePadControl : MonoBehaviour
+{
+    private void Awake()
+    {
+        UnityEngine.Rendering.DebugManager.instance.enableRuntimeUI = false;
+    }
+}
